48ab6c3476f6c238b3569efdfc7a580fcc4c4f98
mustafa.genc@hotmail.co.uk <mustafa.genc@hotmail.co.uk>
2025-09-27 19:50:49 +0100

    Created the guest class

---
+++ Models/Guest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.Models
+{
+    public int GuestId {  get; set; }
+    public string FullName { get; set; }
+    public string Email {  get; set; } 
+    public string PhoneNUmber { get; set; }
+    public ICollection<Reservation> Reservations { get; set; }
+}
