cab200a926618c5a52e11ec08201a2e7dff36808
mustafa.genc@hotmail.co.uk <mustafa.genc@hotmail.co.uk>
2025-09-27 19:47:20 +0100

    Add project files.

---
+++ Models/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelReservationSystem.Models
+{
+    public int RoomId {get; set; } //PK
+    public string RoomNumber { get; set; }
+    public string RoomType { get; set; } // Single, Double
+    public decimal PricePerNight { get; set; }
+    public ICollection<Reservation> Reservations {  get; set; } // One to Many Reservations
+}
